f8840cb4158ba3cfd22fe5fe099b1fb03c057c2f
ktmgw486 <ktmgw486@gmail.com>
2026-04-01 12:40:50 +0900

    feat:Inquiry 스키마 추가

---
+++ prisma/migrations/20260401014919_add_inquiry_tables/migration.sql
@@ -0,0 +1,50 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[phoneNumber]` on the table `store` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "StatusType" AS ENUM ('PendingAnswer', 'CompletedAnswer');
+
+-- CreateTable
+CREATE TABLE "inquiry" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "status" "StatusType" NOT NULL DEFAULT 'PendingAnswer',
+    "isSecret" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "inquiry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reply" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "inquiryId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reply_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reply_inquiryId_key" ON "reply"("inquiryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "store_phoneNumber_key" ON "store"("phoneNumber");
+
+-- AddForeignKey
+ALTER TABLE "inquiry" ADD CONSTRAINT "inquiry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reply" ADD CONSTRAINT "reply_inquiryId_fkey" FOREIGN KEY ("inquiryId") REFERENCES "inquiry"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reply" ADD CONSTRAINT "reply_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
