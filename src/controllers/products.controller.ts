60b9424bcb55e0782e96f07c37c60745a2e96f91
ktmgw486 <ktmgw486@gmail.com>
2026-04-07 14:38:27 +0900

    feat:변수명 오타수정

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -10,7 +10,7 @@ export async function createInquiry(req:Request, res: Response) {
     productId: productId, // URL에서 가져온 productId를 포함
     userId: userId // 인증된 사용자 ID를 포함    
    };
-   const valiedData = inquiryCreateSchema.parse(dateToValidate); // Zod를 사용하여 데이터 검증
-   const newInquiry = await productsService.createInquiry(valiedData); // 실제로는 인증된 사용자 ID와 문의 대상 상품 ID를 가져와야 합니다.
+   const validatedData = inquiryCreateSchema.parse(dateToValidate); // Zod를 사용하여 데이터 검증
+   const newInquiry = await productsService.createInquiry(validatedData); // 실제로는 인증된 사용자 ID와 문의 대상 상품 ID를 가져와야 합니다.
    res.status(201).json(newInquiry);
 }   
