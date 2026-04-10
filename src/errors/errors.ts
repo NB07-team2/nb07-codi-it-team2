375ea18647f3d39c56f0398f34707aeb6e54bef9
sckim-NB <sckim9654@gmail.com>
2026-04-10 15:07:29 +0900

    fix: error 상속 경우 수정

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,31 +1,31 @@
-import { CustomError } from "./customError";
+import { CustomError } from './customError';
 
 export class BadRequestError extends CustomError {
-  constructor(message: string = "잘못된 요청입니다.") {
+  constructor(message: string = '잘못된 요청입니다.') {
     super(message, 400);
   }
 }
 
 export class UnauthorizedError extends CustomError {
-  constructor(message: string = "인증이 필요합니다.") {
+  constructor(message: string = '인증이 필요합니다.') {
     super(message, 401);
   }
 }
 
 export class ForbiddenError extends CustomError {
-  constructor(message: string = "접근 권한이 없습니다.") {
+  constructor(message: string = '접근 권한이 없습니다.') {
     super(message, 403);
   }
 }
 
 export class NotFoundError extends CustomError {
-  constructor(message: string = "요청하신 정보를 찾을 수 없습니다.") {
+  constructor(message: string = '요청하신 정보를 찾을 수 없습니다.') {
     super(message, 404);
   }
 }
 
 export class ConflictError extends CustomError {
-  constructor(message: string = "이미 존재하는 데이터입니다.") {
+  constructor(message: string = '이미 존재하는 데이터입니다.') {
     super(message, 409);
   }
 }
@@ -50,7 +50,7 @@ export class InvalidRequestError extends BadRequestError {
   }
 }
 
-export class InvalidCredentialsError extends NotFoundError {
+export class InvalidCredentialsError extends UnauthorizedError {
   constructor() {
     super('존재하지 않거나 비밀번호가 일치하지 않습니다');
   }
