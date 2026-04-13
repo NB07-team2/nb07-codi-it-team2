56921de95225b4d0bfdf12c7d9a9a072908956b7
sckim-NB <sckim9654@gmail.com>
2026-04-13 17:46:54 +0900

    feat: ZodError 발생 에러 추가

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from './customError';
+import { ZodError } from 'zod';
 
 export const errorHandler = (
   err: Error,
@@ -7,18 +8,28 @@ export const errorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
+  if (err instanceof ZodError || err.name === 'ZodError') {
+    return res.status(400).json({
+      statusCode: 400,
+      message: '잘못된 요청입니다.',
+      error: 'Bad Request',
+    });
+  }
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({
+      statusCode: err.statusCode,
       message: err.message,
     });
   }
   if (err.name === 'StructError') {
     return res.status(400).json({
+      statusCode: 400,
       message: '잘못된 요청입니다.',
     });
   }
   console.error('예상치 못한 에러 발생:', err);
   return res.status(500).json({
+    statusCode: 500,
     message: '서버 내부 에러가 발생하였습니다.',
   });
 };
