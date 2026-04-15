1b3d55fa15e60b1293005e1dcc391844891779f5
ktmgw486 <ktmgw486@gmail.com>
2026-04-15 18:14:40 +0900

    내문의 수정 및 삭제 api 구현

--- src/controllers/inquiry.controller.ts
+++ src/controllers/inquiry.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as inquiryService from '../services/inquiry.service';
 import { create } from 'superstruct';
-import { getInquiriesMyListStruct } from '../structs/inquiry.struct';
+import { getInquiriesMyListStruct, inquiryUpdateSchema } from '../structs/inquiry.struct';
 
 export async function myInquiryList(req: Request, res: Response) {
   const { id: userId , type: userType } = req.user!
@@ -20,3 +20,18 @@ export async function getInquiryDetail(req: Request, res: Response) {
     const inquiryDetail = await inquiryService.getInquiryDetail(inquiryId, userId, userType);
     res.status(200).json(inquiryDetail);
 } 
+
+export async function updateInquiry(req: Request, res: Response) {
+    const inquiryId  = req.params.id as string;
+    const { id: userId, type: userType } = req.user!;
+    const validatedData = inquiryUpdateSchema.parse(req.body); 
+    const updatedInquiry = await inquiryService.updateInquiry(inquiryId, userId, validatedData, userType);
+    res.status(200).json(updatedInquiry);
+} 
+
+export async function deleteInquiry(req: Request, res: Response) {
+    const inquiryId  = req.params.id as string;
+    const { id: userId, type: userType } = req.user!;
+    const deletedInquiry = await inquiryService.deleteInquiry(inquiryId, userId, userType);
+    res.status(200).json(deletedInquiry);
+}
