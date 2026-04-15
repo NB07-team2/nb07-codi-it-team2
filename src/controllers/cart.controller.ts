9f5c05a0aa9e9509e7e1eaa1a660da45c05ff822
김관규 <kimgg9310@naver.com>
2026-04-15 11:31:47 +0900

    feat:장바구니 삭제 api 구현 완료

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,16 +1,13 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler.util";
 import * as cartService from "../services/cart.service";
-import { updateCartSchema } from "../structs/cart.struct";
+import { updateCartSchema, deleteCartItemSchema } from "../structs/cart.struct";
 
 export const createCart = asyncHandler(async (req: Request, res: Response) => {
-  // 미들웨어에서 검증된 user 객체 사용 (Non-null assertion 사용)
   const user = req.user!;
 
-  // 서비스에 user 전체를 넘겨서 서비스가 권한 및 로직을 처리하게 함
   const cart = await cartService.createCart(user);
 
-  // 서비스에서 가공된 DTO를 그대로 응답
   res.status(201).json(cart);
 });
 
@@ -31,4 +28,13 @@ export const updateCart = asyncHandler(async (req: Request, res: Response) => {
   const updatedItems = await cartService.updateCart(user, productId, sizes);
   
   res.status(200).json(updatedItems);
+});
+
+export const deleteCartItem = asyncHandler(async (req: Request, res: Response) => {
+  const user = req.user!;
+  
+  const { cartItemId } = deleteCartItemSchema.parse(req.params);
+
+  await cartService.deleteCartItem(user, cartItemId);
+  res.status(204).send();
 });
