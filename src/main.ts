c377997b4c7d76faba8377ef5668bb890526d1f0
Bora Lee <dltngus1317@gmail.com>
2026-04-28 10:35:10 +0900

    refactor: SSE 방식 확정에 따른 http.createServer 제거

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { PORT } from './utils/constants.util';
-import { createServer } from 'http';
 import app from './app';
 
-const server = createServer(app);
-//웹소켓 구현된거 나중에 추가.
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
